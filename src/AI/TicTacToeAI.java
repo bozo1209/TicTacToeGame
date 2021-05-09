66e002cd612b024073438d825add2b0e16ac1a1d
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2021-05-09 22:41:34 +0200

    adding alpha beta pruning

--- src/AI/TicTacToeAI.java
+++ src/AI/TicTacToeAI.java
@@ -20,9 +20,10 @@ public class TicTacToeAI {
                 if (gameBoard.getGameBoard()[i][j].equals("")){
 //                    System.out.println("GameBoard.getInstance().getGameBoard()[i][j].equals(\"\")");
                     gameBoard.setGameBoard(i, j, "o");
-                    int score = minimax(gameBoard, 0, true);
-                    System.out.println("if i: " + i + " j: " + j);
-                    System.out.println("score: " + score + " bestscore: " + bestScore);
+                    count =0;
+                    int score = minimax(gameBoard, 0, true, Integer.MIN_VALUE, Integer.MAX_VALUE);
+//                    System.out.println("if i: " + i + " j: " + j);
+//                    System.out.println("score: " + score + " bestscore: " + bestScore);
 //                    System.out.println(Arrays.deepToString(gameBoard.getGameBoard()));
 //                    System.out.println("bestMove() " + score);
 //                    System.out.println("bestscore " + bestScore);
@@ -31,7 +32,7 @@ public class TicTacToeAI {
 //                        System.out.println("score > bestScore");
                         bestScore = score;
                         move = new CustomPair(i,j);
-                        System.out.println("best score: " + bestScore);
+//                        System.out.println("best score: " + bestScore);
 //                        break outerLoop;
                     }
                 }
@@ -51,7 +52,7 @@ public class TicTacToeAI {
 
     GameStatus gameStatus = GameBoard.getInstance().resultsOfGame();
 
-    private static int minimax(GameBoard gameBoard, int depth, boolean isMaximizing){
+    private static int minimax(GameBoard gameBoard, int depth, boolean isMaximizing, int alpha, int beta){
         count += 1;
         if (!gameBoard.resultsOfGame().equals(GameStatus.ONGOING)) {
 //            System.out.println("!gameBoard.resultsOfGame().equals(GameStatus.ONGOING)");
@@ -86,17 +87,22 @@ public class TicTacToeAI {
         int bestScore;
         if (isMaximizing){
             bestScore = Integer.MIN_VALUE;
+            outerLoop:
             for (int i = 0; i < gameBoard.getGameBoard().length; i++){
                 for (int j = 0; j < gameBoard.getGameBoard()[i].length; j++){
                     if (gameBoard.getGameBoard()[i][j].equals("")){
                         gameBoard.setGameBoard(i, j, "x");
-                        int score = minimax(gameBoard, depth + 1, false);
+                        int score = minimax(gameBoard, depth + 1, false, alpha, beta);
                         score -= depth;
 //                        System.out.println(Arrays.deepToString(gameBoard.getGameBoard()));
                         gameBoard.setGameBoard(i, j, "");
 //                        System.out.println("if i: " + i + " j: " + j);
 //                        System.out.println("score: " + score + " bestscore: " + bestScore);
                         bestScore = Math.max(score, bestScore);
+                        alpha = Math.max(alpha, bestScore);
+                        if (beta <= alpha){
+                            break outerLoop;
+                        }
 //                        System.out.println("po score: " + score + " bestscore: " + bestScore);
 //                        bestScore = Math.max(score, bestScore) - depth;
 //                        if (score > bestScore){
@@ -109,17 +115,22 @@ public class TicTacToeAI {
             return bestScore;
         }else {
             bestScore = Integer.MAX_VALUE;
+            outerLoop:
             for (int i = 0; i < GameBoard.getInstance().getGameBoard().length; i++){
                 for (int j = 0; j < GameBoard.getInstance().getGameBoard()[i].length; j++){
                     if (gameBoard.getGameBoard()[i][j].equals("")){
                         gameBoard.setGameBoard(i, j, "o");
-                        int score = minimax(gameBoard, depth + 1, true);
+                        int score = minimax(gameBoard, depth + 1, true, alpha, beta);
                         score += depth;
 //                        System.out.println(Arrays.deepToString(gameBoard.getGameBoard()));
                         gameBoard.setGameBoard(i, j, "");
 //                        System.out.println("else i: " + i + " j: " + j);
 //                        System.out.println("score: " + score + " bestscore: " + bestScore);
                         bestScore = Math.min(score, bestScore);
+                        beta = Math.min(beta, bestScore);
+                        if (beta <= alpha){
+                            break outerLoop;
+                        }
 //                        System.out.println("po score: " + score + " bestscore: " + bestScore);
 //                        bestScore = Math.min(score, bestScore) + depth;
 //                        if (score < bestScore){
